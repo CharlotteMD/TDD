935df0c5405aaae092da85c231289af8a57871b2
CharlotteMD <charlottemdavies@hotmail.co.uk>
2018-08-14 13:26:46 +0100

    leap year test complete

--- _test.js
+++ _test.js
@@ -1,3 +1,4 @@
+var isLeapYear = require('./LeapYear');
 var assert = require('assert');
 
 describe('#isLeapYear(year)', () => {
@@ -6,17 +7,12 @@ describe('#isLeapYear(year)', () => {
     {year: 1996, leapYear: true},
     {year: 2000, leapYear: true},
     {year: 1999, leapYear: false},
-    {year: 1900, leapYear: false}
+    {year: 1900, leapYear: false},
+    {year: 1800, leapYear: false},
+    {year: 2400, leapYear: true}
   ].forEach((testCase) => {
     it(`should ${testCase.leapYear ? 'pass' : 'fail'} ${testCase.year} as a leap year`, () => {
       assert.equal(isLeapYear(testCase.year), testCase.leapYear);
     });
   });
 }); //what we are testing within the function.
-
-var isLeapYear = (year) => {
-  if (year === 1900) {
-    return false
-  }
-  return year % 4 === 0;
-};
